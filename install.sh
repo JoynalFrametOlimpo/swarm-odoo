acdec705cb195e853c637fd486fffd6e7a1c153e
root <ingjjortega@gmail.com>
2019-12-08 17:00:01 -0500

    [UPD] Preparación de variables de entornoy

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ if [ "$SO" = "centos" ]; then
      echo "(tput setaf 4)***************** UPDATE SO ************************************************$(tput setaf 3)"
      yum -y update
      echo "(tput setaf 4)***************** INSTALLING DEPENDS ***************************************$(tput setaf 3)"
-     yum install -y yum-utils device-mapper-persistent-data lvm2
+     yum install -y yum-utils device-mapper-persistent-data lvm2 --no-install-recommends
      yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
      echo "(tput setaf 4)***************** INSTALL DOCKER ******************************************$(tput setaf 3)"
      yum install -y docker-ce
@@ -31,11 +31,11 @@ if [ "$SO" = "centos" ]; then
      systemctl start docker.service
      yum install -y epel-release
      echo "(tput setaf 4)*************** INSTALL Python-pip ****************************************$(tput setaf 3)"
-     yum install -y python-pip
+     yum install -y python-pip --no-install-recommends
      echo "(tput setaf 4)************** INSTALL DOCKER-COMPOSE *************************************$(tput setaf 3)"
-     pip install docker-compose
+     pip install docker-compose --no-install-recommends
      echo "(tput setaf 4)************* Upgrade Python *********************************************$(tput setaf 3)"
-     yum -y upgrade python
+     yum -y upgrade python --no-install-recommends
      docker version
      docker-compose version
 fi
@@ -46,31 +46,34 @@ if [ "$SO" = "ubuntu" ]; then
      echo "$(tput setaf 4)***************** UPDATE SO ************************************************$(tput setaf 3)"
      apt-get update
      echo "$(tput setaf 4)***************** INSTALL DOCKER ******************************************$(tput setaf 3)"
-     apt-get -y install docker.io
+     apt-get -y install docker.io --no-install-recommends
      echo "$(tput setaf 4)***************** INSTALL DOCKER-COMPOSE******************************************$(tput setaf 3)"
-     apt-get -y install docker-compose
+     apt-get -y install docker-compose --no-install-recommends
+     echo "$(tput setaf 4)***************** INFORMATION DOCKER******************************************$(tput setaf 3)"
      docker version
      docker-compose version
 fi
 
 if [ $environment -eq 1 ]; then
     ln -s compose/compose-production.yml docker-compose.yml
+    cp compose/.envPro .env
 fi
 if [ $environment -eq 2 ]; then
     ln -s compose/compose-develop.yml docker-compose.yml
+    cp compose/.envDev .env
 fi
 
 echo "$(tput setaf 4)******************************* Building image Odoo 13 *********************************$(tput setaf 3)"
 docker build -t odoo:13 .
 
 # Create and stop project
-if [ ! -d /opt/odoo/13 ]; then
-    mkdir -p /opt/odoo/13 
-    echo "$(tput setaf 4)************************** Up Service ****************************************$(tput setaf 3)"
-    docker-compose up -d
-    echo "$(tput setaf 4)************************* Stop Services **************************************$(tput setaf 3)"
-    docker-compose stop
-fi
+#if [ ! -d /opt/odoo/13 ]; then
+#    mkdir -p /opt/odoo/13 
+#    echo "$(tput setaf 4)************************** Up Service ****************************************$(tput setaf 3)"
+#    docker-compose up -d
+#    echo "$(tput setaf 4)************************* Stop Services **************************************$(tput setaf 3)"
+#    docker-compose stop
+#fi
 
 # Copy odoo configuration file in new project
 if [ ! -f /opt/odoo/13/conf/odoo.conf ]; then
@@ -83,7 +86,6 @@ if [ ! -f /opt/odoo/13/nginx/nginx.conf ]; then
    echo "$(tput setaf 4)***************** Copiando archivo nginx.conf en ruta de proyecto*********************$(tput setaf 3)"
    cp ./nginx.conf /opt/odoo/13/nginx
 fi
-
 
 if [ $environment -eq 1 ]; then
 echo "$(tput setaf 4)###########################################################################################$(tput setaf 3)"
@@ -105,11 +107,11 @@ if [ $isDomain -eq 1 ]; then
        echo "$(tput setaf 4)################## Installing Certbot ############ $(tput setaf 3)"
        pip install requests==2.6.0
        easy_install --upgrade pip
-       yum -y install certbot -y
+       yum -y install certbot -y --no-install-recommends
 
     fi
     if [ "$SO" = "ubuntu" ]; then
-       apt-get install certbot
+       apt-get install certbot -y --no-install-recommends
     fi
 
   # Install certificates
@@ -141,7 +143,7 @@ if [ $isDomain -eq 1 ]; then
 
      cp ./certbot-conf/options-ssl-nginx.conf /opt/odoo/13/certbot/conf/
      openssl dhparam -dsaparam -out /opt/odoo/13/certbot/conf/ssl-dhparams.pem 4096
-     echo "$(tput setaf 4)Proceder a cambiar contraseñas en .ENV... y configuracion en /opt/odoo/nginx/nginx.conf $(tput setaf 3)"
+     echo "$(tput setaf 4)Proceder a cambiar contraseñas en .ENV... y configuracion en /opt/odoo/13/nginx/nginx.conf $(tput setaf 3)"
   fi
 fi
 fi
